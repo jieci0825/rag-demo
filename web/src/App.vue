bd39492ece60b33cec52c0ac0c931ec35ca448e1
coderjc <coderjc@qq.com>
2026-05-26 00:43:11 +0800

    feat: frontend project init

---
+++ web/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="container">
+        <h1>hello rag</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+    width: 100%;
+}
+</style>
